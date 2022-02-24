0b3c51fc23e310878cdbc4cc22d9fa947d5e53f3
allbani <allbanibagas@gmail.com>
2022-02-24 18:26:51 +0700

    add praktikum

--- 04_Introduction to Algorithm and Dart Programming/Praktikum/praktikum.dart
+++ 04_Introduction to Algorithm and Dart Programming/Praktikum/praktikum.dart
@@ -1,1 +1,23 @@
+void main() {
+  String teks1 = "CONTOH SOAL!!!!";
+  String teks = "Implementasi Rumus Luas Lingkaran";
+  String rumus = "phi x r x r";
+  double contoh = 3.14;
+  String contoh1 = "Jari Jari";
+  int soal = 50;
+  String soal1 =
+      "Diketahui sebuah lingkaran memiliki diameter $soal cm. Berapakah luas lingkaran tsb?";
+  int soal2 = 50;
+  var soal3 = 50 / 2;
+  double hasil = contoh * soal3 * soal3;
 
+  print(teks);
+  print("Rumus = $rumus");
+  print("phi = $contoh");
+  print("r = $contoh1");
+  print(teks1);
+  print(soal1);
+  print("d = $soal2 cm");
+  print("r = d/2 = $soal3");
+  print("Diketahui bahwa luas lingkarannya adalah -> $hasil");
+}
