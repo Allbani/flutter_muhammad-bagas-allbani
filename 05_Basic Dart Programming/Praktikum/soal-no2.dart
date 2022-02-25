a04ecfd8df386f564ad1d8c7e49c49010f56fe3e
allbani <allbanibagas@gmail.com>
2022-02-25 14:37:58 +0700

    add praktikum dan screenshots

---
+++ 05_Basic Dart Programming/Praktikum/soal-no2.dart
@@ -0,0 +1,7 @@
+void main() {
+  final String name = "Bagas";
+  final String hobi = "Running";
+  final String asal = "Bandung";
+
+  print("Nama saya adalah $name, Hobi saya $hobi, Saya berasal dari $asal");
+}
