2b61b66857b08c80d3881343e48bb42c5347b145
allbani <allbanibagas@gmail.com>
2022-03-02 13:10:30 +0700

    update praktikum

--- 06_Branching - Looping - Function/Praktikum/soal no1.dart
+++ 06_Branching - Looping - Function/Praktikum/soal no1.dart
@@ -1,1 +1,14 @@
+// TAKS 1
+void main() {
+  int nilai = 90;
+  if (nilai > 70) {
+    print('Nilainya adalah A');
+  } else if (nilai > 40) {
+    print('Nilainya adalah B');
+  } else if (nilai > 0) {
+    print('Nilainya adalah C');
+  } else {
+    print('tidak memiliki nilai');
+  }
+}
 
---
+++ 06_Branching - Looping - Function/Praktikum/soal no2.dart
@@ -0,0 +1,10 @@
+// TAKS 2
+
+void main(List<String> arguments) {
+  int n = 10;
+  int faktorial = 1;
+  for (var i = 1; i <= n; i++) {
+    faktorial *= i;
+  }
+  print(faktorial);
+}
