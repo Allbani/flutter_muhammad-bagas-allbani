697273bcba8bacd2e571fc9f40ebe248fe28e7e3
allbani <allbanibagas@gmail.com>
2022-03-04 16:11:22 +0700

    add praktikum dan screenshots

--- 07_Advance Function - Async-Await/Praktikum/soal1.dart
+++ 07_Advance Function - Async-Await/Praktikum/soal1.dart
@@ -1,1 +1,21 @@
+void main() async {
+  await hitungList(list: [2, 4, 6], pengali: 2);
+  tes();
+}
+
+Future<void> hitungList({required List list, required int pengali}) async {
+  await Future.delayed(Duration(seconds: 1),(){
+    final hasilList = [];
+    for (var isiList in list) {
+    isiList = isiList * pengali;
+    hasilList.add(isiList);
+  }
+  print(hasilList.toString());
+  });
+  
+}
+
+void tes() {
+  print('haii');
+}
 
