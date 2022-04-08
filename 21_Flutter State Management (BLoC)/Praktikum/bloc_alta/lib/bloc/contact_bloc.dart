006734671fc1a229cd9f6d5a1dd3095241a39236
allbani <allbanibagas@gmail.com>
2022-04-08 13:04:02 +0700

    update contac_bloc

--- 21_Flutter State Management (BLoC)/Praktikum/bloc_alta/lib/bloc/contact_bloc.dart
+++ 21_Flutter State Management (BLoC)/Praktikum/bloc_alta/lib/bloc/contact_bloc.dart
@@ -3,26 +3,34 @@ import 'package:bloc_alta/model/contact_data.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ContactBloc extends Bloc<ContactEvent, List<ContactData>> {
-  ContactBloc() : super([]);
+  ContactBloc() : super([]) {
+    List<ContactData> listContact = [];
 
-  Stream<List<ContactData>> mapEventToState(ContactEvent event) async* {
-    switch (event.event) {
-      case Event.add:
-        List<ContactData> newState = List.from(state);
-        // ignore: unnecessary_null_comparison
-        if (event.contactData != null) {
-          newState.add(event.contactData);
-        }
-        yield newState;
-        break;
-      case Event.delete:
-        List<ContactData> newState = List.from(state);
-        print(newState.length);
-        newState.removeAt(event.contactIndex);
-        yield newState;
-        break;
-      default:
-        throw Exception('Not found $event');
-    }
+    on<ContactEvent>((event, emit) {
+      listContact.add(event.contactData);
+      emit(listContact);
+    });
   }
 }
+
+
+// Stream<List<ContactData>> mapEventToState(ContactEvent event) async* {
+//   switch (event.event) {
+//     case Event.add:
+//       List<ContactData> newState = List.from(state);
+//       // ignore: unnecessary_null_comparison
+//       if (event.contactData != null) {
+//         newState.add(event.contactData);
+//       }
+//       yield newState;  
+//       break;
+//     case Event.delete:
+//       List<ContactData> newState = List.from(state);
+//       print(newState.length);
+//       newState.removeAt(event.contactIndex);
+//       yield newState;
+//       break;
+//     default:
+//       throw Exception('Not found $event');
+//   }
+// }
--- 21_Flutter State Management (BLoC)/Praktikum/bloc_alta/lib/events/contact_event.dart
+++ 21_Flutter State Management (BLoC)/Praktikum/bloc_alta/lib/events/contact_event.dart
@@ -1,19 +1,13 @@
 import 'package:bloc_alta/model/contact_data.dart';
 
-enum Event { add, delete }
+enum Event { add }
 
 class ContactEvent {
   late final ContactData contactData;
-  late final int contactIndex;
   late final Event event;
 
   ContactEvent.add(ContactData contactData) {
     event = Event.add;
     contactData = contactData;
-  }
-
-  ContactEvent.delete(int index) {
-    event = Event.delete;
-    contactIndex = index;
   }
 }
